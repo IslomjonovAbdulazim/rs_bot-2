4e9eb05f374e223a6e6979466af6efbf888ea73b
Abdurahmon Muminov <155967262+Mortistubea@users.noreply.github.com>
2026-01-06 20:57:30 +0500

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,8 @@
 BOT_TOKEN = "8579757300:AAEy8fUxoISogUMCAPnaPyBJLr5PPsMY-pI"
 ADMINS  = [6379557413]
+
+SPREADSHEET_NAME = "RAHIMOV SCHOOL"  # Google Sheets dagi nom bilan bir xil
+CREDENTIALS_FILE = "credentials.json"  # 👈 Bu nom yuklab olgan faylingiz bilan bir xil bo'lishi kerak
+
+HEADER_COLOR = {"red": 0.29, "green": 0.53, "blue": 0.91}
+SUCCESS_COLOR = {"red": 0.58, "green": 0.77, "blue": 0.49}
